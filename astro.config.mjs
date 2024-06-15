2c07be01765b0d100a3d9a1c2b77cdc8af1aec75
Nikita Liakhovka <n.liakhovka@gmail.com>
2024-06-15 16:31:38 +0900

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  output: "hybrid",
+  adapter: netlify(),
 });
