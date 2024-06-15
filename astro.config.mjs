6d1e5ed6baa5167be44d5dbad2af11712cf7bf84
Nikita Liakhovka <n.liakhovka@gmail.com>
2024-06-15 22:10:25 +0900

    wip i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from "astro/config";
-
+import paraglide from "@inlang/paraglide-astro";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  site: "https://gg56.com",
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+  ],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ko"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
