758cc3233acf0724bbf660d87b1a8f631dd4b261
Nikita Liakhovka <n.liakhovka@gmail.com>
2025-07-07 16:41:13 +0900

    Add astro-icon dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -16,4 +18,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
