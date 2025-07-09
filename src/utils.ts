464cf57235f183dd8b7e512b0e6522a0913c78c3
Nikita Liakhovka <n.liakhovka@gmail.com>
2025-07-09 17:39:34 +0900

    feat: implement getIconNameFromUrl function for dynamic icon mapping

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,22 @@ import { LOCALES } from "./constants";
 export function getLocaleParams(): GetStaticPathsResult {
   return LOCALES.map((locale) => ({ params: { locale } }));
 }
+
+export function getIconNameFromUrl(href: string) {
+  const iconMap = {
+    linkedin: "linkedin-in",
+    facebook: "square-facebook",
+    instagram: "square-instagram",
+    wiki: "wikipedia-w",
+    twitter: "x-twitter",
+    "x.com": "x-twitter",
+  };
+
+  for (const [key, value] of Object.entries(iconMap)) {
+    if (href.includes(key)) {
+      return `fa6-brands:${value}`;
+    }
+  }
+
+  return "lucide:globe";
+}
