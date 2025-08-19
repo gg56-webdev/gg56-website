532a16e02e1e75219d069bba43d1d0399b81fc2c
Nikita Liakhovka <n.liakhovka@gmail.com>
2025-08-19 16:08:18 +0900

    fix: business one pager

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -4,11 +4,6 @@ const translations = {
       "GG56 is driving global change with innovative tech solutions. Partner with us to make a difference. GG56 Limited - For a Global Good.",
   },
   page: {
-    it: "IT",
-    fintech: "FIN-Tech",
-    blockchain: "Blockchain",
-    exchange: "Crypto Exchange",
-    "digital-bank": "Digital Bank",
     "advisory-board": "Advisory Board",
     about: "About",
     "founders-message": "Founders' Message",
@@ -16,37 +11,7 @@ const translations = {
   },
   nav: {
     home: "Home",
-    business: "Business",
     about: "About",
-  },
-  it: {
-    h1: "Our Solutions",
-    app: {
-      h2: "FingeRate App",
-      p: "FingeRate’s survey app is revolutionizing the way you take surveys by enabling you to earn rewards while providing valuable feedback to businesses. Our app is the first of its kind, based on the metaverse, and provides you with a real-world map that offers exciting opportunities to earn rewards for taking surveys. Say goodbye to time wasted on surveys that do not offer any added value and start earning rewards today with FingeRate!",
-      ul: [
-        "Reward-based Survey",
-        "Real-world Map",
-        "Anywhere & Anytime",
-        "Free!",
-      ],
-      cta: "Start Now!",
-    },
-    bot: {
-      h2: "FingeRate Bot",
-      p: "With our FingeRate Ecosystem , we go beyond just providing businesses with customer data. Our touchless system ensures a safe and convenient experience for your customers, while our real-time data analysis helps you gain a deeper understanding of their behavior.",
-      cta: "Learn More",
-    },
-    passngo: {
-      h2: "Pass&Go",
-      p: "Pass&Go’s primary service is the generation of digital credentials in the form of a QR code. These credentials verify that you meet certain indicators such as testing negative or possessing immunity, which enable you to fully re-engage in social and economic activities. With Pass&Go, you no longer have to worry about carrying physical documents or worrying about misplaced or damaged documents.",
-      cta: "Learn More",
-    },
-    signis: {
-      h2: "SIGNIS FR",
-      p: "SIGNIS FR is the world’s first Catholic metaverse platform. It serves as a hub for global individuals to connect, interact, and unite under the common mission of Signis (International Association of Catholic Communications). SIGNIS FR brings together the digitally-savvy MZ generation of around 5 billion people. Through this platform, individuals from diverse backgrounds, including Catholics and metaverse enthusiasts worldwide, can transcend the limitations of time and space to foster understanding, empathy, and diverse experiences, thereby promoting Catholicism and working towards global peace.",
-      cta: "Learn More",
-    },
   },
   about: {
     h1: "About",
